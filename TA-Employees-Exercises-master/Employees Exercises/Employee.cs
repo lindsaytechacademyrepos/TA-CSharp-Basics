7e0b82694526cc26670006f7d3307128fa04b543
Lindsay Alexander <linmalex@gmail.com>
2018-09-28 17:32:00 -0700

    add employees exercise

---
+++ TA-Employees-Exercises-master/Employees Exercises/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees_Exercises
+{
+    class Employee<T> : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+
+        public void Quit()
+        {
+            throw new NotImplementedException();
+        }
+
+        //public static bool operator ==(Employee employee1, Employee employee2)
+        //{
+        //    return employee1.Id == employee2.Id;
+        //}
+
+        //public static bool operator !=(Employee employee1, Employee employee2)
+        //{
+        //    return employee1.Id == employee2.Id;
+        //}
+
+        public int Id { get; set; }
+        public List<T> Things { get; set; }
+    }
+}
---
+++ TA-Employees-Exercises-master/Employees Exercises/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee employee1 = new Employee() { FirstName = "Sample", LastName = "Student 1", Id = 88776 };
+            //Employee employee2 = new Employee() { FirstName = "Sample", LastName = "Student 2", Id = 887 };
+            //Employee employee3 = new Employee() { FirstName = "Sample", LastName = "Student 3", Id = 88776 };
+
+            //employee1.SayName();
+
+            //IQuittable quittable = new Employee();
+            ////quittable.Quit();
+
+            Employee<string> employeeString = new Employee<string>();
+            Employee<int> employeeInt = new Employee<int>();
+
+            employeeString.Things = new List<string>();
+            employeeString.Things.Add("string1");
+            employeeString.Things.Add("string2");
+            employeeString.Things.Add("string3");
+            employeeString.Things.Add("string4");
+
+            employeeInt.Things = new List<int>();
+            employeeInt.Things.Add(1);
+            employeeInt.Things.Add(2);
+            employeeInt.Things.Add(3);
+            employeeInt.Things.Add(4);
+
+            for (int i = 0; i < employeeInt.Things.Count; i++)
+            {
+                Console.WriteLine("Employee int value: " + employeeInt.Things[i]);
+                Console.WriteLine("Employee string value: "+employeeString.Things[i]);
+            }
+
+            Console.Read();
+        }
+    }
+}
